1d1f6ad8bbc3dd40ef314b770a828ee7cc256dd4
nevindra <nevindraibnazhifi@gmail.com>
2024-07-28 16:17:00 +0700

    minor edit

--- components/profile/commentSection.tsx
+++ components/profile/commentSection.tsx
@@ -1,36 +1,28 @@
 import type { UserType } from '@/types/userType';
 import { Divider } from '@nextui-org/divider';
-import { Spinner } from '@nextui-org/spinner';
 import { CommentInput } from './commentInput';
 import { CommentBox } from './commentList';
 
-export const CommentSection = ({
-	questionId,
-	user,
-	refetchQuestion,
-	loading
-}: {
-	questionId: string;
-	user: UserType | null;
-	refetchQuestion: () => void;
+export const CommentSection = ({ 
+	questionId, 
+	user, 
+	refetchQuestion 
+}: { 
+	questionId: string, 
+	user: UserType | null,
+	refetchQuestion: () => void
 }) => {
 	return (
 		<>
-			<Divider className="my-2" />
-			<div className="w-full space-y-2">
-				{loading ? (
-					<Spinner /> // Render Spinner if loading
-				) : (
-					<>
-						<CommentBox id={questionId} />
-						<CommentInput
-							question_id={questionId}
-							user={user}
-							onCommentAdded={refetchQuestion}
-						/>
-					</>
-				)}
-			</div>
+		<Divider className="my-2" />
+		<div className="w-full space-y-2">
+			<CommentBox id={questionId} />
+			<CommentInput 
+				question_id={questionId} 
+				user={user} 
+				onCommentAdded={refetchQuestion}
+			/>
+		</div>
 		</>
 	);
-};
+}
